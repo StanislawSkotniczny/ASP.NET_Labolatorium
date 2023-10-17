426da3f5687034bd986a683ef0957fe7842eae89
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-10-17 20:26:44 +0200

    first task done

---
+++ Labolatorium_2/Controllers/CalculatorController.cs
@@ -0,0 +1,67 @@
+﻿using Labolatorium_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labolatorium_2.Controllers
+{
+
+    public enum Operators
+    {
+        ADD, SUB, MUL, DIV
+    }
+
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Result([FromQuery(Name = "operator")] Operators? op, double? x, double? y)
+        //{
+        //    if (op == null || x == null || y == null)
+        //    {
+        //        return View("Error");
+        //    }
+
+
+        //    double? result = 0;
+        //    switch (op)
+        //    {
+        //        case Operators.SUB:
+        //            result = x - y;
+        //            break;
+        //        case Operators.ADD:
+        //            result = x + y;
+        //            break;
+        //        case Operators.DIV:
+        //            result = x / y;
+        //            break;
+        //        case Operators.MUL:
+        //            result = x * y;
+        //            break;
+        //    }
+        //    ViewBag.Result = result;
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public IActionResult Result([FromForm] Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Labolatorium_2/Models/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using Labolatorium_2.Controllers;
+
+namespace Labolatorium_2.Models
+{
+    public class Calculator
+    {
+        public Operators? Operator { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+
+        public String Op
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case Operators.ADD:
+                        return "+";
+                    case Operators.SUB:
+                        return "-";
+                    case Operators.DIV:
+                        return "/";
+                    case Operators.MUL:
+                        return "*";
+
+                    default:
+                        return "";
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            return Operator != null && X != null && Y != null;
+        }
+
+        public double Calculate()
+        {
+            switch (Operator)
+            {
+                case Operators.ADD:
+                    return (double)(X + Y);
+                case Operators.SUB:
+                    return (double)(X - Y);
+                case Operators.DIV:
+                    return (double)(X / Y);
+                case Operators.MUL:
+                    return (double)(X * Y);
+
+                default: return double.NaN;
+            }
+        }
+    }
+}
