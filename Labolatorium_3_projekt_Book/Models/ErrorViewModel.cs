e05be821820861820fb5b6ccdb135e6e7ff9278e
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-10-24 17:02:05 +0200

    setting up book proj

---
+++ Labolatorium_3_projekt_Book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Labolatorium_3_projekt_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
