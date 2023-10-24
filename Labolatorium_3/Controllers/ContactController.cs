486ba94b7603be23da017bb21e68a9202afde0f7
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-10-24 16:47:45 +0200

    final

---
+++ Labolatorium_3/Controllers/ContactController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Labolatorium_3.Models;
+
+namespace Labolatorium_3.Controllers
+{
+    
+    
+        public class ContactController : Controller
+        {
+            static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+            static int id = 0;
+            public IActionResult Index()
+            {
+
+                return View(_contacts);
+            }
+
+            [HttpGet]
+            public IActionResult Create()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Create(Contact model)
+            {
+                if (ModelState.IsValid)
+                {
+                    // zapisanie modelu do bazy lub kolekcji
+                    model.Id = id++;
+                    _contacts[model.Id] = model;
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            [HttpGet]
+            public IActionResult Update(int id)
+            {
+                return View(_contacts[id]);
+            }
+            [HttpPost]
+            public IActionResult Update(Contact model)
+            {
+                if (ModelState.IsValid)
+                {
+                    // zapisanie modelu do bazy lub kolekcji
+                    _contacts[model.Id] = model;
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            [HttpGet]
+            public IActionResult Delete(int id)
+            {
+                return View(_contacts[id]);
+            }
+            [HttpPost]
+            public IActionResult Delete(Contact model)
+            {
+                _contacts.Remove(model.Id);
+                return RedirectToAction("Index");
+            }
+            [HttpGet]
+            public IActionResult Details()
+            {
+
+                return View();
+                return RedirectToAction("Index");
+            }
+        }
+    
+}
---
+++ Labolatorium_3/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Labolatorium_3.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Imię jest wymagane!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Wprowadzone imię jest za długie, wprowadź maksymalnie do 50 znaków.")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        public DateTime? Birth { get; set; }
+
+    }
+}
