8a5f369d1e0a0652734f3b0c6c7cfe7c64e41911
stanirl <115420426+StanislawSkotniczny@users.noreply.github.com>
2023-11-05 09:56:58 +0100

    Delete Labolatorium_3_projekt_Book directory

--- Labolatorium_3_projekt_Book/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Labolatorium_3_projekt_Book.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
