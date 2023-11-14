9ec53bcf1bb5ec24a5f640c30fc021dcadd8b4b0
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-11-14 20:32:29 +0100

    lab5 progress

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,7 +17,7 @@ namespace Data
         {
             var  folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-            Path = System.IO.Path.Join(path, "contacts.db");
+            Path = System.IO.Path.Join(path, "books.db");
 
         }
 
@@ -30,15 +30,20 @@ namespace Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ContactEntity>().HasData(
-                new ContactEntity() { Id = 1, Name = "adam", Email = "asdad@.pl", Phone = "23123123131" } ,
+                new ContactEntity() { Id = 1, Name = "adam", Email = "asdad@.pl", Phone = "23123123131" },
                 new ContactEntity() { Id = 2, Name = "ada21123m", Email = "asdad@.pl", Phone = "23123123131" },
                 new ContactEntity() { Id = 3, Name = "a231111dam", Email = "asdad@.pl", Phone = "23123123131" });
+
+            modelBuilder.Entity<BookEntity>().HasData(
+                new BookEntity() { Id = 1, Author = "sssss", Title = "ssadasdad", ISBN="1234567891234" , PageNumber="222" , PublicationYear="2002-09-09" , Publisher="loll"});
+                
         }
 
 
 
 
 
-        public DbSet<ContactEntity>Contacts { get; set; }
+        public DbSet<ContactEntity> Contacts { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
     }
 }
---
+++ Data/Migrations/20231114193136_BookInitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    PageNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    ISBN = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    PublicationYear = table.Column<string>(type: "TEXT", nullable: false),
+                    Publisher = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "books",
+                columns: new[] { "Id", "Author", "ISBN", "PageNumber", "PublicationYear", "Publisher", "Title" },
+                values: new object[] { 1, "sssss", "1234567891234", "222", "2002-09-09", "loll", "ssadasdad" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+        }
+    }
+}
